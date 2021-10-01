6a5d4d81f7969141a0048930b1306b02653d9ca0
Muhammed Khaled <muhammed.webdeveloper@gmail.com>
2021-10-01 13:58:41 +0200

    update 3 - respon

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -32,7 +32,9 @@ const Home: React.SFC<Props> = (props) => {
 
   const mintTokens = async () => {
     if (!loginStatus) {
-      toast.error("Unsupported Network. This platform is working on Ethereum Network");
+      toast.error(
+        "Unsupported Network. This platform is working on Ethereum Network"
+      );
       return;
     }
     const load_toast_id = toast.loading("Please wait for Mint...");
@@ -75,46 +77,59 @@ const Home: React.SFC<Props> = (props) => {
             <p className="innerBtnTextR">MINT</p>
           </div>
         </div>
-        <br />
+        {/* <br /> */}
         <p>
-          <strong>GENIE QUEST IS THE first nft</strong>
+          <strong>
+            Genie Quest is the first NFT of its kind. <br /> A lottery NFT with
+            an opportunity to win the jackpot!{" "}
+          </strong>
         </p>
         <br />
         <p>
-          <strong>how does genie quest work</strong>
+          <strong>how does genie quest work?</strong>
+        </p>
+
+        <p>
+          It’s always about the treasure! Simple way to win the jackpot is by
+          summoning the legendary genie s which will grant your wish of riches!
+          The legendary Genie is summoned on gold and appears with an iridescent
+          background. (There are 10 Legendary Genies)
         </p>
         <br />
         <p>
-          it's always about the treasure! simple way to win the jackpot <br />{" "}
-          by summoning the ulimate genie! <br /> (there are 10 legendary)
+          1. Connect your wallet to Metamask make sure you are on Ethereum
+          network.
         </p>
-        <br />
-        <p>1. mint an nft for 0.05 eth</p>
-        <p>2. wait for the genie summon </p>
+        <p>2. Press Mint to begin minting a Genie Quest NFT</p>
         <p>
-          3. if you have a legendary (1 of 10) you win a big prize directly{" "}
-          <br /> from the genie quest{" "}
+          3.3. Rub the lamp and wait for the NFT minting and transaction to
+          process.
+        </p>
+        <p>
+          4. If you mint any legendary (any of the 10) you win a big prize
+          directly from the Genie Quest according to the payment schedule.{" "}
+        </p>
+        <p>5. Join our social communities and share your victorious summon.</p>
+        <br />
+        <p>
+          The more minted Genies that are common, <br /> rare or premium the
+          better chance you have at a legendary NFT jackpot! <br /> Maximum 10
+          Mint per wallet!
         </p>
       </div>
       <div className="legendary_list">
-        <p>legendary #1 minted:$1500</p>
         <p>legendary #1 minted:$2000</p>
-        <p>legendary #1 minted:$3000</p>
-        <p>legendary #1 minted:$4000</p>
-        <p>legendary #1 minted:$5000</p>
-        <p>legendary #1 minted:$6000</p>
-        <p>legendary #1 minted:$7000</p>
-        <p>legendary #1 minted:$8000</p>
-        <p>legendary #1 minted:$9000</p>
-        <p>legendary #1 minted:$25,000!</p>
+        <p>legendary #2 minted:$3000</p>
+        <p>legendary #3 minted:$4000</p>
+        <p>legendary #4 minted:$5000</p>
+        <p>legendary #5 minted:$6000</p>
+        <p>legendary #6 minted:$7000</p>
+        <p>legendary #7 minted:$8000</p>
+        <p>legendary #8 minted:$9000</p>
+        <p>legendary #9 minted:$10000</p>
+        <p>legendary #10 minted:$25,000!</p>
       </div>
-      <div className="home_text">
-        <p>
-          the more minted genines that are common, rare, or <br /> premuium the
-          better chance you have at a legendary nft jackpot!
-        </p>
-        <p>maximum 10 mint per wallet</p>
-      </div>
+      <div className="home_text"></div>
       <div className="starList">
         <img src={telegram} alt="star" className="star" />
         <img src={twitter} alt="star" className="star" />
